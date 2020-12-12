39687fa7fa0d450da4fe052e733fa1354309e474
Aldous Nicolas <aldous.darcy@live.com>
2020-12-12 18:28:36 +0800

    added react-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './redux/store';
+import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+import { store, persistor } from "./redux/store";
+
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
