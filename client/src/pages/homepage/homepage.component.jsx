4abeb8418f377a177e39bb625779a4d372d894d6
Aldous Nicolas <aldous.darcy@live.com>
2020-12-26 21:50:24 +0800

    Profiling

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { HomePageContainer } from "./homepage.styles.jsx";
 
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuraton) => {
+        console.log({
+          id,
+          phase,
+          actualDuraton,
+        });
+      }}
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
